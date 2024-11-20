b071abfe5705cda941304ddbae46269ecdeed6fc
Viking26LV <sprat2626@gmail.com>
2024-11-19 16:57:50 -0800

    person2 commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,8 +1,8 @@
 public class test {
     public static void main(String[] args) {
 
-        System.out.println("This is the project version 5");
+        System.out.println("This is the project version 6 made by person2");
 
     }
 }
-// done with version fifth
+// done with version six
